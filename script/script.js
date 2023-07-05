31b027b4543d4e2bc751697317fc0122f739a3e4
VladimirMakarof <stan13.09@mail.ru>
2023-07-05 22:54:21 +0300

    05.07

--- script/script.js
+++ script/script.js
@@ -320,19 +320,19 @@ function closeAllPopups(overlay) {
 
 // Инициализация Swiper
 document.addEventListener('DOMContentLoaded', function() {
-    // Swiper initialization code here
+
     const swiper = new Swiper('.swiper', {
-        // Optional parameters
+
         direction: 'horizontal',
         loop: true,
 
-        // If we need pagination
+
         pagination: {
             el: '.swiper-pagination',
             clickable: true,
         },
 
-        // Navigation arrows
+
         navigation: {
             nextEl: '.swiper-button-next',
             prevEl: '.swiper-button-prev',
